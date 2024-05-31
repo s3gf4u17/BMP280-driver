e888578aeca626620a8826c026113089d09c8317
s3gf4u17 <szymon.kolakowski@gmail.com>
2024-05-31 19:19:27 +0200

    sensor spi setup

--- bmp280_driver.c
+++ bmp280_driver.c
@@ -12,4 +12,15 @@
 #define bmp280_pin_miso 12
 #define bmp280_pin_cs 13
 
+void bmp280_setup() {
+    spi_init(bmp280_port_spi,1000000);
+    spi_set_format(bmp280_port_spi,8,SPI_CPOL_0,SPI_CPHA_0,SPI_MSB_FIRST);
+    gpio_set_function(bmp280_pin_sclk,GPIO_FUNC_SPI);
+    gpio_set_function(bmp280_pin_mosi,GPIO_FUNC_SPI);
+    gpio_set_function(bmp280_pin_miso,GPIO_FUNC_SPI);
+    gpio_init(bmp280_pin_cs);
+    gpio_set_dir(bmp280_pin_cs,GPIO_OUT);
+    gpio_put(bmp280_pin_cs,1);
+}
+
 #endif
