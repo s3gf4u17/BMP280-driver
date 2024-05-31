a015c3127d9c178979916a67138c66a8a4bb72c9
s3gf4u17 <szymon.kolakowski@gmail.com>
2024-05-31 19:19:16 +0200

    define sensor connection

--- bmp280_driver.c
+++ bmp280_driver.c
@@ -6,4 +6,10 @@
 #include "hardware/spi.h"
 #include "hardware/gpio.h"
 
+#define bmp280_port_spi spi1
+#define bmp280_pin_sclk 10
+#define bmp280_pin_mosi 11
+#define bmp280_pin_miso 12
+#define bmp280_pin_cs 13
+
 #endif
